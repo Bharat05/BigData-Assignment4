4cb76513066cfe19bed4b6566e964405c01fe436
Bharat05 <bthakur05@outlook.com>
2020-11-11 21:37:08 -0600

    copy_of_class_example

--- bt_test_server.py
+++ bt_test_server.py
@@ -6,6 +6,46 @@ import socket
 from threading import Thread
 
 
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(
+            bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
+
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name + ": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+
 # Defining constants
 clients = {}
 addresses = {}
@@ -14,50 +54,12 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 33000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
-# should we call this socket?
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER.bind(ADDR)
 
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print(f"{client_address} has connected.")
-        client.send(bytes("Greetings from Houston!"
-                          "Please type your name and press enter!",
-                          "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
-
-
-def handle_client(client):
-    """ Handles a single client connection."""
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = (f"Welcome {name}! If you ever want to quit, type 'quit'"
-               "to quit.")
-    msg = (f"{name} has joing the chat!")
-    client.send(bytes(msg, "utf8"))
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("quit", "utf8"):
-            broadcast(msg, name + ": ")
-        else:
-            client.send(bytes("quit", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes(f"{name} has left the chat.", "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification
-    """ Broadcasts a message to all the client. """
-    for client in clients:
-        client.send(bytes(prefix, "utf8") + msg)
-
-
 if __name__ == "__main__":
-    SERVER.listen(5)  # Listens for 5 Connections
+    SERVER.listen(5)
     print("Waiting for connection...")
     ACCEPT_THREAD = Thread(target=accept_incoming_connections)
     ACCEPT_THREAD.start()
